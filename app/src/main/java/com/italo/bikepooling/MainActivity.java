8a879cf6b14a5fb70c53478b6531b4d85aa03fb2
Italo Wieczorek <italoaw@gmail.com>
2017-11-25 16:49:00 -0200

    Activities de signin e signup.

---
+++ app/src/main/java/com/italo/bikepooling/MainActivity.java
@@ -0,0 +1,21 @@
+package com.italo.bikepooling;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+        auth.signOut();
+
+    }
+}
