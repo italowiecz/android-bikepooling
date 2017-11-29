fcd54c4a2e1278616cc4ae1993d2c82087fe8ed7
Italo Wieczorek <italoaw@gmail.com>
2017-11-28 22:27:44 -0200

    TOp menu.

--- app/src/main/java/com/italo/bikepooling/MainActivity.java
+++ app/src/main/java/com/italo/bikepooling/MainActivity.java
@@ -1,7 +1,11 @@
 package com.italo.bikepooling;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -14,8 +18,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        auth = FirebaseAuth.getInstance();
-        auth.signOut();
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+/*        auth = FirebaseAuth.getInstance();
+        auth.signOut();*/
 
     }
 }
